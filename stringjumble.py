8e7baeacdb870e1e96822bf6dfb5609d00c5e10e
sarahdunbar <sarahdunbar@hanovernorwichschools.org>
2015-09-29 14:22:48 -0400

    Updated from Brython Server: 9/29/2015 2:22:48 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
-Author: Eric
-Credit: Me!
+Author: Sarah Dunbar
+Credit: http://stackoverflow.com/questions/743806/split-string-into-a-list-in-python
 
 Assignment:
 
@@ -25,3 +25,5 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
+string = input("Please enter a string of text (the bigger the better):")
