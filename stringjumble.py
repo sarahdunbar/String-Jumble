cc398afcd781a58e31edc6462e263085c3892990
sarahdunbar <sarahdunbar@hanovernorwichschools.org>
2015-10-02 14:12:14 -0400

    Updated from Brython Server: 10/2/2015 2:12:13 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,7 +26,7 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
-string = input("Please enter a string of text (the bigger the better):")
+string = input("Please enter a string of text (the bigger the better): ")
 print ('You entered "' + string + '". Now jumble it: ') 
 strlist = string.split(" ")
 r = len(strlist)
